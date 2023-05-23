de97523b52fff42edaa634fd59e416d65b38c8f4
shreyaa2506 <97473022+shreyaa2506@users.noreply.github.com>
2023-05-23 23:57:35 +0530

    Time: 3 ms (98.9%), Space: 14.2 MB (27.23%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,34 +1,60 @@
 class Solution {
 public:
     void bfs(int n,int m,int row, int col,vector<vector<int>>& ans,vector<vector<int>>& image,int ic,int color,int dr[],int dc[]){
-        ans[row][col] = color;
+        // ans[row][col] = color;
+        // queue<pair<int,int>>q;
+        // q.push({row,col});
+        // while(q.size() != 0){
+        //     int r = q.front().first;
+        //     int c = q.front().second;
+        //     q.pop();
+        //     for(int i=0;i<4;i++){
+        //         int nr = r + dr[i]; /////neighbour row
+        //         int nc = c + dc[i]; /////neighbour col
+        //         if(nr >= 0 && nr < n && nc >=0 && nc < m && ans[nr][nc] != color && image[nr][nc] == ic){
+        //             ans[nr][nc] = color;
+        //             q.push({nr,nc});
+        //         }
+        // }
+        // }
+        ans[row][col]=color;
         queue<pair<int,int>>q;
         q.push({row,col});
-        while(q.size() != 0){
+        while(q.size()!=0){
             int r = q.front().first;
             int c = q.front().second;
             q.pop();
-            for(int i=0;i<4;i++){
-                int nr = r + dr[i]; /////neighbour row
-                int nc = c + dc[i]; /////neighbour col
-                if(nr >= 0 && nr < n && nc >=0 && nc < m && ans[nr][nc] != color && image[nr][nc] == ic){
-                    ans[nr][nc] = color;
+            for(int i =0;i<4;i++){
+                int nr = r+dr[i];
+                int nc = c+ dc[i];
+                if(nr>=0 && nr<n && nc<m && nc>=0 && ans[nr][nc]!=color && image[nr][nc]==ic){
+                    ans[nr][nc]=color;
                     q.push({nr,nc});
                 }
-        }
+            }
         }
         
     }
 
 
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        // int n = image.size();
+        // int m = image[0].size();
+        // int ic = image[sr][sc]; //// initial color
+        // vector<vector<int>>ans = image; /// 2-D vector ans is acting as a visited array
+        // int dr[] = {-1,0,1,0};  // for traversing matrix w.r.t a paricular cell
+        // int dc[] = {0,1,0,-1};
+        // bfs(n,m,sr,sc,ans,image,ic,color,dr,dc);
+        // return ans; 
+
         int n = image.size();
-        int m = image[0].size();
-        int ic = image[sr][sc]; //// initial color
-        vector<vector<int>>ans = image; /// 2-D vector ans is acting as a visited array
-        int dr[] = {-1,0,1,0};  // for traversing matrix w.r.t a paricular cell
-        int dc[] = {0,1,0,-1};
+        int m = image[0] .size();
+        int ic= image[sr][sc];
+        vector<vector<int>>ans = image;
+        int dr[]={-1,0,1,0};
+        int dc[]={0,1,0,-1};
         bfs(n,m,sr,sc,ans,image,ic,color,dr,dc);
-        return ans;  
+        return ans;
     }
+    
 };
