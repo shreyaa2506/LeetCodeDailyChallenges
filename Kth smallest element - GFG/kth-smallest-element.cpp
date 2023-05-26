914b125b677172531b3f2d5b45ac3afdf710dd09
shreyaa2506 <97473022+shreyaa2506@users.noreply.github.com>
2023-05-27 00:42:15 +0530

    Added solution - LeetHub

--- Kth smallest element - GFG/kth-smallest-element.cpp
+++ Kth smallest element - GFG/kth-smallest-element.cpp
@@ -14,17 +14,31 @@ class Solution{
     // r : ending index of the array i.e size-1
     // k : find kth smallest element and return using this function
     int kthSmallest(int arr[], int l, int r, int k) {
-        priority_queue<int,vector<int>,greater<int>>minheap;
-        for(int i =l; i<=r;i++){
-            minheap.push(arr[i]);
+        
+        priority_queue<int>pq;
+        for(int i =l;i<k;i++){
+            pq.push(arr[i]);
+            
         }
-        int size1 = minheap.size();
-        int size2= minheap.size();
-        while(size1>size2-k+1){
-            minheap.pop();
-            size1--;
+        
+        for(int i =k;i<=r;i++){
+            if(arr[i]<pq.top()){
+                pq.pop();
+                pq.push(arr[i]);
+            }
         }
-        return minheap.top();
+        return pq.top();
+        // priority_queue<int,vector<int>,greater<int>>minheap;
+        // for(int i =l; i<=r;i++){
+        //     minheap.push(arr[i]);
+        // }
+        // int size1 = minheap.size();
+        // int size2= minheap.size();
+        // while(size1>size2-k+1){
+        //     minheap.pop();
+        //     size1--;
+        // }
+        // return minheap.top();
     }
 };
 
