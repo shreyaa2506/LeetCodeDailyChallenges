0a5cd7c15581d64aa56b174081bd2605bde7fd06
shreyaa2506 <97473022+shreyaa2506@users.noreply.github.com>
2023-06-02 23:06:22 +0530

    Time: 100 ms (84.58%), Space: 14.3 MB (89.99%) - LeetHub

--- 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.cpp
+++ 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.cpp
@@ -1,21 +1,23 @@
-#define ll long long
+
 class Solution {
 public:
-    void dfs(int src, int&cnt,vector<int>&vis,vector<int>adj[]){
+    #define ll long long
+  
+
+    void dfs(int src, int& cnt, vector<int>&vis,vector<int>adj[]){
         vis[src]=1;
         cnt++;
         for(int x:adj[src]){
-            if(vis[x]==0){
-                dfs(x,cnt,vis,adj);
-            }
+            if(vis[x]==0)
+            dfs(x,cnt,vis,adj);
         }
     }
     int maximumDetonation(vector<vector<int>>& bombs) {
         int n = bombs.size();
         vector<int>adj[n];
-        for(int i =0;i<n;i++){
+        for(int i =0; i<n;i++){
             ll x1 = bombs[i][0];
-            ll y1=bombs[i][1];
+            ll y1 = bombs[i][1];
             ll r1 = bombs[i][2];
             for(int j =0; j<n;j++){
                 if(i!=j){
@@ -24,21 +26,61 @@ public:
                     ll r2 = bombs[j][2];
                     ll dsq = (x1-x2)*(x1-x2)+(y1-y2)*(y1-y2);
                     ll p = r1;
-                    if(dsq<=p*p){
+                    if (dsq<=r1*r1){
                         adj[i].push_back(j);
                     }
                 }
-
             }
         }
         vector<int>vis(n);
         int ans = 0;
         for(int i =0; i<n;i++){
-            int cnt =0;
+            int cnt = 0;
             dfs(i,cnt,vis,adj);
             ans = max(ans,cnt);
             fill(vis.begin(),vis.end(),0);
         }
         return ans;
     }
+
+    // void dfs(int src, int&cnt,vector<int>&vis,vector<int>adj[]){
+    //     vis[src]=1;
+    //     cnt++;
+    //     for(int x:adj[src]){
+    //         if(vis[x]==0){
+    //             dfs(x,cnt,vis,adj);
+    //         }
+    //     }
+    // }
+    // int maximumDetonation(vector<vector<int>>& bombs) {
+    //     int n = bombs.size();
+    //     vector<int>adj[n];
+    //     for(int i =0;i<n;i++){
+    //         ll x1 = bombs[i][0];
+    //         ll y1=bombs[i][1];
+    //         ll r1 = bombs[i][2];
+    //         for(int j =0; j<n;j++){
+    //             if(i!=j){
+    //                 ll x2 = bombs[j][0];
+    //                 ll y2 = bombs[j][1];
+    //                 ll r2 = bombs[j][2];
+    //                 ll dsq = (x1-x2)*(x1-x2)+(y1-y2)*(y1-y2);
+    //                 ll p = r1;
+    //                 if(dsq<=p*p){
+    //                     adj[i].push_back(j);
+    //                 }
+    //             }
+
+    //         }
+    //     }
+    //     vector<int>vis(n);
+    //     int ans = 0;
+    //     for(int i =0; i<n;i++){
+    //         int cnt =0;
+    //         dfs(i,cnt,vis,adj);
+    //         ans = max(ans,cnt);
+    //         fill(vis.begin(),vis.end(),0);
+    //     }
+    //     return ans;
+    // }
 };
